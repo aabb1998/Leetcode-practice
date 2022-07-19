6815b74f902c2d2bd53275e3bf5bba104746997f
Abdul Abbou <53510613+aabb1998@users.noreply.github.com>
2022-07-20 08:44:27 +1000

    Time: 0 ms (100.00%), Space: 43.6 MB (26.93%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -2,20 +2,13 @@ class Solution {
     public int[] runningSum(int[] nums) {
         
         int currCount = 0;
-        int[] store = new int[nums.length];
         
         for (int i = 0; i < nums.length; i++) {
             currCount = nums[i] + currCount;
-            store[i] = currCount; 
+            nums[i] = currCount;
         }
         
-        return store;
+        return nums;
     }
 }
 
-
-// [1,2,3,4]
-//    - 
-// set currCount to nums[i] + currCount
-// increment i
-// repeat
