1240639ec00aa5346bbf42f951562b34d58b330e
Abdul Abbou <53510613+aabb1998@users.noreply.github.com>
2022-07-27 15:16:15 +1000

    Time: 3 ms (7.07%), Space: 54.8 MB (19.85%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -5,21 +5,19 @@ class Solution {
         int end = nums.length - 1;
         int mid;
         
-        while (start <= end) {
+        while ( start <= end) {
             
             mid = start + (end - start) / 2;
-            if (nums[mid] == target) return mid;
-            if (target < nums[mid]) {
-                end = mid - 1;
-                
-            } else {
-                start = mid + 1;
-            }
              
+            if (nums[mid] == target) return mid;
+            if (target > nums[mid]) {
+                start = start + 1;
+            } else {
+                end = mid - 1;
+            }
+        
             
         }
-        
         return -1;
-        
     }
 }
